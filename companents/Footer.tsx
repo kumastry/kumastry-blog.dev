3cf2ef424c683b0c55f34d549dd3e962f55273fd
kumastry <chma19063@g.nihon-u.ac.jp>
2023-02-21 17:38:50 +0900

    headerの追加

---
+++ companents/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import type { ReactNode } from 'react';
+
+const Footer = () : JSX.Element => {
+    return (
+        <div>
+        </div>
+    );
+
+}
+
+export default Footer;
--- companents/Layout.tsx
+++ companents/Layout.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import type { ReactNode } from 'react';
+import Header from "./Header";
+//import Footer from "./Footer";
 
 const Layout = ({children}:{ children: ReactNode }) : JSX.Element => {
     return (
+        
         <div style={{margin:"30px"}}>
             {children}
         </div>
