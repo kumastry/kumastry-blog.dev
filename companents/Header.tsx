9fb1046589f28fe8a5b826bcf2606636edad52e7
kumastry <chma19063@g.nihon-u.ac.jp>
2023-02-25 23:24:50 +0900

    navの追加

--- companents/Header.tsx
+++ companents/Header.tsx
@@ -7,6 +7,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+import Botton from '@mui/material/Button';
 
 const Header = (): JSX.Element => {
   return (
@@ -16,6 +18,17 @@ const Header = (): JSX.Element => {
           <Typography style={{ color: "black" }} variant="h4" component="div">
             kumastry.dev
           </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            
+              
+              <Botton sx={{ my: 3, color: 'black', display: 'block' }}>Blog</Botton >
+
+              <Botton sx={{ my: 2, color: 'black', display: 'block' }}>About me</Botton >
+              
+            
+          </Box>
+
         </Toolbar>
       </Container>
     </AppBar>
