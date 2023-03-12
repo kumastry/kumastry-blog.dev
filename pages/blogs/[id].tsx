3450ecd688e7200b89d98ca32976a896dd889e72
kumastry <chma19063@g.nihon-u.ac.jp>
2023-03-13 08:46:26 +0900

    render tocの導入

--- pages/blogs/[id].tsx
+++ pages/blogs/[id].tsx
@@ -1,10 +1,12 @@
 import type { GetStaticProps, GetStaticPaths } from "next";
 import { client } from "@/libs/client";
 import type { Blog } from "@/libs/types";
-import { Eyecatch } from '../../libs/types';
+import { renderToc } from '@/libs/render-toc';
+import { Eyecatch, Category } from '../../libs/types';
 
 import styles from "./blogs.module.css";
 import Image from "next/image";
+
 
 // Generates `/posts/1` and `/posts/2`
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -34,12 +36,18 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 export default function Blog({ data }: any) {
   //console.log(data);
-
+  const toc = renderToc(data.content);
+  console.log(toc); // 検証用にconsole.logでデバッグ
   return (
     <main>
-      <Image className = {styles.image}src={data.eyecatch.url} alt={data.title}  fill />
+      <Image className = {styles.image} src={data.eyecatch.url} alt={data.title}  fill />
+      <br/><br/><br/>
       <h1 className="title">{data.title}</h1>
       <p>{data.publishedAt}</p>
+      <p>{data.category.name}</p>
+
+      <br/><br/>
+
       <div
         dangerouslySetInnerHTML={{
           __html: `${data.content}`,
